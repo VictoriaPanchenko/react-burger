ff3595250b5054fdfe2b4ce877eb5d90b952468c
Victoria Panchenko <vkpanchenko@gmail.com>
2022-05-11 23:31:26 +0200

    remove inline styles

--- src/components/ingredients-navigation/ingredients-navigation.jsx
+++ src/components/ingredients-navigation/ingredients-navigation.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import navigationStyles from './ingredients-navigation.module.css';
 
 const IngredientsNavigation = ( {tabs} ) => {
     const [current, setCurrent] = useState(tabs[0]);
 
     return (
-        <nav style={{ display: 'flex' }}>
+        <nav className={navigationStyles.navigation}>
             {
                 tabs.map((item, index) => 
                 <Tab key={index} value={item} active={current === item} onClick={setCurrent}>{item}</Tab> )
