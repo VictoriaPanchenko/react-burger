34f863c53a5686b9c754b66409d77262ca03c952
Victoria Panchenko <vkpanchenko@gmail.com>
2022-07-09 14:39:01 +0200

    feat: set roating in header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,35 +1,60 @@
 import React from "react";
-import {BurgerIcon, ListIcon, Logo, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import appHeaderStyles from './app-header.module.css';
 
 const AppHeader = () => {
+
+    const { pathname } = useLocation();
+    const { user } = useSelector(store => store.user);
+
+
     return (
         <header className={`${appHeaderStyles.header} pt-4 pb-4`} >
             <nav className={appHeaderStyles.navigation}>
                 <div className={appHeaderStyles.wrapper}>
                     <menu className={appHeaderStyles.menuList}>
                         <li className="pr-5 pl-5">
-                            <a href="#"  className={appHeaderStyles.linkActive}>
-                                <BurgerIcon type="primary"/>
+                            <NavLink
+                                activeClassName={appHeaderStyles.linkActive}
+                                className={`${appHeaderStyles.link} text text_type_main-default`}
+                                exact
+                                to="/"
+                            >
+                                <BurgerIcon type="primary" />
                                 <p className="text text_type_main-default ml-2">Конструктор</p>
-                            </a>
+
+                            </NavLink>
                         </li>
                         <li className="pr-5 pl-5 ml-2">
-                            <a href="#" className={appHeaderStyles.link}>
-                                <ListIcon type='secondary'/>
+                            <NavLink
+                                activeClassName={appHeaderStyles.linkActive}
+                                className={`${appHeaderStyles.link} text text_type_main-default`}
+                                exact
+                                to="/profile/orders"
+                            >
+                                <ListIcon type='secondary' />
                                 <p className="text text_type_main-default ml-2">Лента заказов</p>
-                            </a>
+                            </NavLink>
                         </li>
                     </menu>
-                    <Logo/>                    
+                    <Link to="/">
+                        <Logo />
+                    </Link>
                 </div>
                 <div className="pr-5 pl-5">
-                    <a href="#" className={appHeaderStyles.link}>
-                        <ProfileIcon type='secondary'/>
+                    <NavLink
+                        activeClassName={appHeaderStyles.linkActive}
+                        className={`${appHeaderStyles.link} text text_type_main-default`}
+                        exact
+                        to="/profile/orders"
+                    >
+                        <ProfileIcon type='secondary' />
                         <p className="text text_type_main-default ml-2">Личный кабинет</p>
-                    </a>
+                    </NavLink>
                 </div>
-            </nav>            
+            </nav>
         </header>
     );
 }
