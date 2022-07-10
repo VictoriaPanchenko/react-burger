110182e6dc8b3964b682bea7c00c3778a6e034f0
Victoria Panchenko <vkpanchenko@gmail.com>
2022-07-11 00:54:48 +0200

    fix: roating

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-      <BrowserRouter basename="/stellar-burger">
+      <BrowserRouter basename="/react-burger">
     <Provider store={store}>
     <App />
     </Provider>
