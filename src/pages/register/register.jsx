d158d3ddace16ce26ce853c89904f4c4e354bd17
Victoria Panchenko <vkpanchenko@gmail.com>
2022-07-10 21:53:22 +0200

    feat: register

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,13 +1,20 @@
-import React, { useState, useRef } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Input,
   Button,
   PasswordInput,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './register.module.css';
+import { createNewUser } from "../../services/actions/user";
 
 export const RegisterPage = () => {
+
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -25,9 +32,21 @@ export const RegisterPage = () => {
     setPassword(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!userName || !email || !password) {
+      return;
+    }
+    dispatch(createNewUser(userName, email, password));
+  };
+
+  useEffect(() => {
+    user && history.push('/');
+  }, [user, history]);
+
   return (
     <main className={styles.wrapper}>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <h1 className={`${styles.title} text text_type_main-medium`}>
           Регистрация
         </h1>
