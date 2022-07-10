368c735dab24bd32f53ccb02106c34d7922f542b
Victoria Panchenko <vkpanchenko@gmail.com>
2022-07-10 21:59:06 +0200

    feat: profile form change

--- src/components/profile-form/profile-form.jsx
+++ src/components/profile-form/profile-form.jsx
@@ -1,18 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, Input } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './profile-form.module.css';
+import { getCookie } from "../../services/cookie-setting";
+import { patchUser } from "../../services/actions/user";
+import { useLocation } from 'react-router-dom';
 
 export const ProfileForm = () => {
-  const [name, setName] = useState("");
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
+
+  const [isDataChanged, setIsDataChanged] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const ref = useRef(null);
+  const { user, patchUserRequest, patchUserFailed, isUserChanged, errMessage } = useSelector(
+    (store) => store.user
+  );
+  const accessToken = getCookie('accessToken');
+  const refreshToken = localStorage.getItem('refreshToken');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) {
+      setEmail(user.email);
+      setName(user.name);
+    }
+  }, [user]);
+
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      dispatch(patchUser(`Bearer ${accessToken}`, name, email, password, refreshToken));
+    },
+    [dispatch, name, email, password, accessToken, refreshToken]
+  );
+
+  const handleCancel = useCallback(
+    (e) => {
+      e.preventDefault();
+      setEmail(user.email);
+      setName(user.name);
+      setPassword('');
+      setHasEmailErr(false);
+      setHasNameErr(false);
+      setHasPasswordErr(false);
+      setIsDataChanged(false);
+    },
+    [user.name, user.email]
+  );
+
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit} name='profile'>
       <Input
+        ref={ref}
         type="text"
         placeholder="Имя"
-        onChange={(evt) => setName(evt.target.value)}
+        onChange={(evt) => {evt.target.value === user.name ? setIsDataChanged(false) : setIsDataChanged(true); setName(evt.target.value)}}
         icon="EditIcon"
         value={name}
         name="name"
@@ -21,35 +67,39 @@ export const ProfileForm = () => {
         size="default"
       />
       <Input
+        ref={ref}
         type="email"
         placeholder="Логин"
-        onChange={(evt) => setLogin(evt.target.value)}
+        onChange={(evt) => {evt.target.value === user.email ? setIsDataChanged(false) : setIsDataChanged(true); setEmail(evt.target.value)}}
         icon="EditIcon"
-        value={login}
-        name="name"
+        value={email}
+        name="login"
         error={false}
         errorText="Ошибка"
         size="default"
       />
       <Input
+        ref={ref}
         type="password"
         placeholder="Пароль"
-        onChange={(evt) => setPassword(evt.target.value)}
+        onChange={(evt) => {evt.target.value === password ? setIsDataChanged(false) : setIsDataChanged(true); setPassword(evt.target.value)}}
         icon="EditIcon"
         value={password}
-        name="name"
+        name="password"
         error={false}
         errorText="Ошибка"
         size="default"
       />
+      {isDataChanged && (
       <div className={styles.buttons_container}>
-        <Button type="secondary" size="medium">
+        <Button onClick={handleCancel} type="secondary" size="medium">
           Отмена
         </Button>
-        <Button type="primary" size="medium">
+        <Button type="primary" size="medium" htmlType="submit"  name='profile'>
           Сохранить
         </Button>
       </div>
+        )}
     </form>
   );
 };
